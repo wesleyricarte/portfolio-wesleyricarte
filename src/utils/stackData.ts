27ae86de9cceca2f3f1676aa008d9a839f0d00a0
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-05-19 14:27:11 -0300

    feat: updating user stacks

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,40 +1,34 @@
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaGit, FaPython } from "react-icons/fa";
+import { TbBrandNextjs } from 'react-icons/tb';
 import {
-  FaDocker,
-  FaHtml5,
-  FaCss3Alt,
-  FaJs,
-  FaNode,
-  FaReact,
-} from "react-icons/fa";
+	SiPostgresql,
+	SiTypescript,
+	SiInsomnia,
+	SiDjango,
+} from 'react-icons/si';
+import { FaGit, FaPython } from 'react-icons/fa';
+import {
+	FaHtml5,
+	FaCss3Alt,
+	FaJs,
+	FaNode,
+	FaReact,
+	FaFigma,
+	FaSass,
+} from 'react-icons/fa';
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
-  { title: "TypeScript", img: SiTypescript },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
-  {
-    title: "React",
-    img: FaReact,
-  },
-  { title: "Next.js", img: TbBrandNextjs },
-  { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: SiPostgresql },
-  { title: "Git", img: FaGit },
-  { title: "Python", img: FaPython },
+	{ title: 'HTML', img: FaHtml5 },
+	{ title: 'CSS', img: FaCss3Alt },
+	{ title: 'JavaScript', img: FaJs },
+	{ title: 'TypeScript', img: SiTypescript },
+	{ title: 'Python', img: FaPython },
+	{ title: 'Django REST Framework', img: SiDjango },
+	{ title: 'Insomnia', img: SiInsomnia },
+	{ title: 'PostgreSQL', img: SiPostgresql },
+	{ title: 'React', img: FaReact },
+	{ title: 'Sass', img: FaSass },
+	{ title: 'Next.js', img: TbBrandNextjs },
+	{ title: 'Node JS', img: FaNode },
+	{ title: 'Git', img: FaGit },
+	{ title: 'Figma', img: FaFigma },
 ];
