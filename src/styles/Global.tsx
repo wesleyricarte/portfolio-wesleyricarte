48ce8454bcadbd53df7a32782f555235b1cc9b9a
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-05-22 12:49:22 -0300

    feat: updating primary color

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -84,8 +84,8 @@ export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
 
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#00bdbd",
+      brand2: "#005b5b",
 
       grey0: "#0A0A0B",
       grey1: "#121214",
