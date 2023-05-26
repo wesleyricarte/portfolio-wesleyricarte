ecbba33536d1389a1ade46f14a9c18d0a30d5926
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-05-26 15:51:23 -0300

    feat: updating projetct's language to portuguese

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,113 +1,129 @@
 // Styles
-import { Container, Flex } from "@/styles/Global";
-import { Text } from "@/styles/Text";
-import { Button } from "@/styles/Buttons";
+import { Container, Flex } from '@/styles/Global';
+import { Text } from '@/styles/Text';
+import { Button } from '@/styles/Buttons';
 
 // Components
-import { Stack } from "@/components/Stack";
-import { Project } from "@/components/Project";
-import { Contacts } from "@/components/Contacts";
+import { Stack } from '@/components/Stack';
+import { Project } from '@/components/Project';
+import { Contacts } from '@/components/Contacts';
 
 // Data
-import { stackData } from "@/utils/stackData";
-import { userData } from "@/utils/userData";
+import { stackData } from '@/utils/stackData';
+import { userData } from '@/utils/userData';
 
-import { FaGithub } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa';
 
 // Page Style
 import {
-  Header,
-  HeaderContent,
-  HeaderButtonsArea,
-  UserImage,
-  StackCards,
-  ProjectsArea,
-  ProjectsAreaSocialMediaMessage,
-  ProjectAreaWrapperColumns,
-  ProjectsAreaContent,
-} from "./style";
+	Header,
+	HeaderContent,
+	HeaderButtonsArea,
+	UserImage,
+	StackCards,
+	ProjectsArea,
+	ProjectsAreaSocialMediaMessage,
+	ProjectAreaWrapperColumns,
+	ProjectsAreaContent,
+} from './style';
 
 export const Home = (): JSX.Element => {
-  const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+	const gihubUrl = `https://github.com/${userData.githubUser}`;
+	const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
 
-  return (
-    <main id="home">
-      <Header>
-        <Container>
-          <HeaderContent>
-            <Flex>
-              <UserImage
-                src={`https://github.com/${userData.githubUser}.png`}
-                alt={userData.nameUser}
-                title={userData.nameUser}
-                width={"48px"}
-                height={"48px"}
-              />
-              <Text color="grey4">Wellcome, I'm {userData.nameUser}</Text>
-            </Flex>
-            <Text as="h1" type="heading1" color="grey5">
-            I{" "}
-              <Text as="span" type="heading1" color="brand1">
-                love
-              </Text>{" "}
-              creating and{" "}
-              <Text as="span" type="heading1" color="brand1">
-                developing
-              </Text>{" "}
-              projects
-            </Text>
-            <Text type="body1" color="grey2">
-              Discover here in this environment, created especially for you, all
-              my projects and technologies
-            </Text>
-            <HeaderButtonsArea>
-              <Button as="a" type="primary" href="#projects">
-                See Projects
-              </Button>
-              <Button as="a" type="outline" target="_blank" href={portfolioUrl}>
-                See my portfolio source code
-              </Button>
-              <Button
-                color="grey5"
-                as="a"
-                css={{ "&:hover": { color: "$grey1" } }}
-                type="circle"
-                target="_blank"
-                href={gihubUrl}
-              >
-                <FaGithub />
-              </Button>
-            </HeaderButtonsArea>
-            <StackCards>
-              {stackData.map((stack, index) => (
-                <Stack key={index} title={stack.title} icon={stack.img} />
-              ))}
-            </StackCards>
-          </HeaderContent>
-        </Container>
-      </Header>
-      <ProjectsArea id="projects">
-        <Container>
-          <ProjectAreaWrapperColumns>
-            <ProjectsAreaSocialMediaMessage>
-              <Text as="h2" type="heading4" color="grey4">
-                My projects
-              </Text>
-              <Text as="p" type="body1" color="grey2">
-                Some of my{" "}
-                <Text as="span" color="brand5">
-                  side projects
-                </Text>
-              </Text>
-            </ProjectsAreaSocialMediaMessage>
-            <ProjectsAreaContent>
-              <Project />
-            </ProjectsAreaContent>
-          </ProjectAreaWrapperColumns>
-        </Container>
-      </ProjectsArea>
-      <Contacts />
-    </main>
-  );
+	return (
+		<main id="home">
+			<Header>
+				<Container>
+					<HeaderContent>
+						<Flex>
+							<UserImage
+								src={`https://github.com/${userData.githubUser}.png`}
+								alt={userData.nameUser}
+								title={userData.nameUser}
+								width={'48px'}
+								height={'48px'}
+							/>
+							<Text color="grey4">
+								Bem-vindos! Eu sou o {userData.nameUser}
+							</Text>
+						</Flex>
+						<Text as="h1" type="heading1" color="grey5">
+							Eu{' '}
+							<Text as="span" type="heading1" color="brand1">
+								sou apaixonado
+							</Text>{' '}
+							por{' '}
+							<Text as="span" type="heading1" color="brand1">
+								criar
+							</Text>{' '}
+							e{' '}
+							<Text as="span" type="heading1" color="brand1">
+								desenvolver
+							</Text>{' '}
+							novos projetos
+						</Text>
+						<Text type="body1" color="grey2">
+							Descubra neste ambiente, criado especialmente pra
+							você, alguns dos meus projetos e tecnologias de
+							trabalho
+						</Text>
+						<HeaderButtonsArea>
+							<Button as="a" type="primary" href="#projects">
+								Visaulizar Projetos
+							</Button>
+							{/* <Button
+								as="a"
+								type="outline"
+								target="_blank"
+								href={portfolioUrl}
+							>
+								See my portfolio source code
+							</Button> */}
+							<Button
+								color="grey5"
+								as="a"
+								css={{ '&:hover': { color: '$grey1' } }}
+								type="circle"
+								target="_blank"
+								href={gihubUrl}
+							>
+								<FaGithub />
+							</Button>
+						</HeaderButtonsArea>
+						<StackCards>
+							{stackData.map((stack, index) => (
+								<Stack
+									key={index}
+									title={stack.title}
+									icon={stack.img}
+								/>
+							))}
+						</StackCards>
+					</HeaderContent>
+				</Container>
+			</Header>
+			<ProjectsArea id="projects">
+				<Container>
+					<ProjectAreaWrapperColumns>
+						<ProjectsAreaSocialMediaMessage>
+							<Text as="h2" type="heading2" color="grey4">
+								Projetos
+							</Text>
+							<Text as="p" type="body1" color="grey2">
+								Algum dos meus{' '}
+								<Text as="span" color="brand5">
+									projetos
+								</Text>
+							</Text>
+						</ProjectsAreaSocialMediaMessage>
+						<ProjectsAreaContent>
+							<Project />
+						</ProjectsAreaContent>
+					</ProjectAreaWrapperColumns>
+				</Container>
+			</ProjectsArea>
+			<Contacts />
+		</main>
+	);
 };
